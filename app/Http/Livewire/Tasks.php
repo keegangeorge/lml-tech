35d3ab897ede71571837506704df8cf9210e5083
Keegan George <kgeorge13@gmail.com>
2021-03-31 11:06:49 -0700

    WIP: editing tasks

--- app/Http/Livewire/Tasks.php
+++ app/Http/Livewire/Tasks.php
@@ -15,15 +15,37 @@ class Tasks extends Component
     public function mount($hasTasks = null)
     {
         // ! TODO: setup finding of current task based on assessment id and add it to the view for the edit path.
-        $this->allTasks = Task::where('assessment_id', 3)->get();
+        $this->allTasks = Task::where('assessment_id', 4)->get();
+        foreach ($this->allTasks as $i => $task) {
+            //     // $this->addTasks = ['title' => $task->title, 'hazards' => $task->hazards, 'riskLevel' => $task->riskLevel, 'controls' => $task->controls];
+
+            //     // echo $task;
+
+            //     // $this->addTasks = [];
+            // echo "This is the index " . $i;
+            // echo "Here is the task " . $task;
+
+            // $this->addTasks = [
+            //     // ['title' => $task->title, 'hazards' => $task->hazards, 'riskLevel' => $task->riskLevel, 'controls' => $task->controls],
+            //     ['title' => $task->title, 'hazards' => $task->hazards, 'riskLevel' => $task->riskLevel, 'controls' => $task->controls]
+            // ];
+
+            $this->addTasks[] = ['title' => $task->title, 'hazards' => $task->hazards, 'riskLevel' => $task->riskLevel, 'controls' => $task->controls];
+            // $this->addTasks = [
+            // ['title' => $task->title, 'hazards' => $task->hazards, 'riskLevel' => $task->riskLevel, 'controls' => $task->controls],
+            // ['title' => $task->title, 'hazards' => $task->hazards, 'riskLevel' => $task->riskLevel, 'controls' => $task->controls]
+            // ];
+        }
+
+
         // $this->addTasks = ?
 
-        if (!is_null($hasTasks)) {
-            $this->allTasks = Task::all();
-            $this->addTasks = [
-                ['title' => '', 'hazards' => '', 'riskLevel' => 'Low', 'controls' => '']
-            ];
-        }
+        // if (!is_null($hasTasks)) {
+        //     $this->allTasks = Task::all();
+        //     $this->addTasks = [
+        //         ['title' => '', 'hazards' => '', 'riskLevel' => 'Low', 'controls' => '']
+        //     ];
+        // }
     }
 
     public function addTask()
